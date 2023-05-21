6439215cc447bfe2bf5d508daefffe99b49b6edd
RISHAV\USER <rishavnandi06@gmail.com>
2023-05-21 19:07:26 +0530

    minor css fixes

--- components/GlassPane.tsx
+++ components/GlassPane.tsx
@@ -3,7 +3,7 @@ import clsx from "clsx"
 const GlassPane = ({ children, className }) => {
   return (
     <div className={clsx(
-        "glass rounded-2xl border-solid border-2 border-gray-200",
+        "glass rounded-2xl border-solid border-2 border-gray-200 gap-8",
         className
       )}
     >
